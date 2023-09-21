fab18e7b73fa6d451a537faf7b09148ef8bfe0a3
AndreyLyulekin <andreilyulekin@yandex.ru>
2023-09-21 17:04:16 -0300

    done with some tests

--- controllers/users.js
+++ controllers/users.js
@@ -7,15 +7,30 @@ module.exports.getUsers = (req, res) => {
 };
 
 module.exports.getUser = (req, res) => {
-  User.findById(req.params.userId)
-    .then((user) => res.send({ data: user }))
-    .catch(() =>
-      res
-        .status(400)
-        .send({
-          message: "Пользователь не найден или переданы некорректные данные",
-        })
-    );
+  // Для уважаемого ревьюрера :)
+  // в документации Монго сказано, что ObjectId — это 12-байтовая
+  // шестнадцатеричная строка типа BSON, что означает что это всегда длинна в 24
+  // символа, но там могут быть как цифры 0-9, так и латинские буквы a-z || A-Z
+  // проверкой на длинну строки можно достичь бага, что в запросе у нас будут
+  // другие символы (.,* и тд), тогда ответ сервера будет приходить,
+  // что пользователь не найден, а это неверный ответ
+  // регулярное выражение помогает решить эту проблему
+  // тк мы знаем что длинна строки всегда 24 символа и мы ее валидируем
+  // можно избежать лишних запросов в БД и сразу вернуть ошибку
+  const objectID = req.params.userId;
+  const validationRegExp = new RegExp(/\w{24}/gm);
+  const isValidate = validationRegExp.test(objectID);
+  if (!isValidate) {
+    return res.status(400).send({ message: "Переданы некорректные данные" });
+  }
+  User.findById(objectID)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: "Пользователь не найден" });
+      }
+      res.send({ data: user });
+    })
+    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
 };
 
 module.exports.createUser = (req, res) => {
