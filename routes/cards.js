2023aeb36917c748b51cd901a3f9ca255de142e9
AndreyLyulekin <andreilyulekin@yandex.ru>
2023-09-21 20:23:45 -0300

    done with some tests

--- routes/cards.js
+++ routes/cards.js
@@ -9,8 +9,8 @@ const {
 
 router.get("/cards", getCards);
 router.post("/cards", createCard);
-router.delete("/cards", deleteCard);
-router.put("/cards", likeCard);
-router.delete("/cards", dislikeCard);
+router.delete("/cards/:cardId", deleteCard);
+router.put("/cards/:cardId/likes", likeCard);
+router.delete("/cards/:cardId/likes", dislikeCard);
 
 module.exports = router;
