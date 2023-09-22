7e682d3b91c293defeeabbb0ff39428de9b499ef
AndreyLyulekin <andreilyulekin@yandex.ru>
2023-09-21 21:21:09 -0300

    done with some tests

--- controllers/cards.js
+++ controllers/cards.js
@@ -27,38 +27,30 @@ module.exports.deleteCard = (req, res) => {
     .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
 };
 
-module.exports.likeCard = (req, res) => {
-  try {
-    Card.findByIdAndUpdate(
-      req.params.cardId,
-      { $addToSet: { likes: req.user._id } },
-      { new: true },
-      (err, card) => {
-        if (err) {
-          throw new Error("Ошибка при лайке карточки");
-        }
-        res.status(200).send(card);
+module.exports.likeCard = (req, res) =>
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true }
+  )
+    .then((card) => {
+      if (!card) {
+        return res.status(404).send({ message: "Карточка не найдена" });
       }
-    );
-  } catch (error) {
-    res.status(400).send({ message: error.message });
-  }
-};
+      res.send({ data: card });
+    })
+    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
 
-module.exports.dislikeCard = (req, res) => {
-  try {
-    Card.findByIdAndUpdate(
-      req.params.cardId,
-      { $pull: { likes: req.user._id } },
-      { new: true },
-      (err, card) => {
-        if (err) {
-          throw new Error("Ошибка при дизлайке карточки");
-        }
-        res.status(200).send(card);
+module.exports.dislikeCard = (req, res) =>
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true }
+  )
+    .then((card) => {
+      if (!card) {
+        return res.status(404).send({ message: "Карточка не найдена" });
       }
-    );
-  } catch (error) {
-    res.status(400).send({ message: error.message });
-  }
-};
+      res.send({ data: card });
+    })
+    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
