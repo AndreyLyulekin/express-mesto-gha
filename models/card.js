9ac9b7767171b768c783d5142c8653e42b4e8836
Andrey Lyulekin <lyulekin@gmail.com>
2023-10-28 16:33:44 -0300

    done with private routes #3

--- models/card.js
+++ models/card.js
@@ -25,34 +25,3 @@ const cardSchema = new mongoose.Schema({
   },
 });
 module.exports = mongoose.model("card", cardSchema);
-
-// const mongoose = require("mongoose");
-
-// const { ObjectId } = mongoose.Schema.Types;
-
-// const cardSchema = new mongoose.Schema({
-//   name: {
-//     type: String,
-//     required: true,
-//     minlength: 2,
-//     maxlength: 30,
-//   },
-//   link: {
-//     type: String,
-//     required: true,
-//   },
-//   owner: {
-//     type: ObjectId,
-//     required: true,
-//   },
-//   likes: {
-//     type: [ObjectId],
-//     required: true,
-//   },
-//   createdAt: {
-//     type: Date,
-//     default: Date.now,
-//   },
-// });
-
-// module.exports = mongoose.model("card", cardSchema);
