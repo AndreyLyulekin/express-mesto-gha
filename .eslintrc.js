927c7d83fde79bb2eddf04d7db562de54e814b61
Andrey Lyulekin <lyulekin@gmail.com>
2023-11-08 18:17:17 -0300

    bug fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,28 +3,25 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: 'airbnb-base',
+  extends: "airbnb-base",
   overrides: [
     {
       env: {
         node: true,
       },
-      files: ['.eslintrc.{js,cjs}'],
+      files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: 'script',
+        sourceType: "script",
       },
     },
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {
-    'no-console': 'off',
-    quotes: [
-      'error',
-      'single',
-    ],
-    'no-underscore-dangle': 'allow',
+    "no-console": "off",
+    quotes: ["error", "single"],
+    "no-underscore-dangle": "0",
   },
 };
